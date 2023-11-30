695bed0c29c970ada6dbf98db3f7faf495fd8089
vtsuellen <vtsuellen@hotmail.com>
2023-11-30 17:20:31 -0300

    feat: classe Character

---
+++ src/Character.ts
@@ -0,0 +1,91 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+const random = getRandomInt(1, 10);
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._dexterity = random;
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._race.maxLifePoints;
+    this._strength = random;
+    this._defense = random;
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: random,
+    };
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy {
+    return { ...this._energy };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._defense;
+
+    if (damage > 0) this._lifePoints -= damage;
+    if (damage <= 0) this._lifePoints -= 1;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += random;
+    this._strength += random;
+    this._dexterity += random;
+    this._defense += random;
+    this._energy.amount = 10;
+
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._lifePoints = this._race.maxLifePoints;
+  }
+
+  special?(): void {
+    this._defense *= 10;
+  }
+}
